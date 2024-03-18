94d4974a6a73421ae8b785e09d926ed23d8848e9
iMuhammadMustafa <29302054+iMuhammadMustafa@users.noreply.github.com>
2024-03-18 10:19:41 -0500

    Add Routers

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use("/public", express.static("public"));
+app.use("/public", express.static("Public"));
+app.use("/styles", express.static("Styles"));
 
 app.listen(3000, () => {
   console.log("Your Server is running on 3000");
@@ -19,4 +20,12 @@ app.listen(3000, () => {
 app.set("view engine", "html");
 app.engine("html", ejs.renderFile);
 
-app.use(router);
+const homepageRouter = require("./Routes/HomepageRoutes");
+const authRouter = require("./Routes/AuthRoutes");
+const postsRouter = require("./Routes/PostsRoutes");
+const adminRouter = require("./Routes/AdminRoutes");
+
+app.use(homepageRouter);
+app.use(authRouter);
+app.use(postsRouter);
+app.use(adminRouter);

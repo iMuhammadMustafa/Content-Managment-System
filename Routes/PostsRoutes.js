d512c46b6a7f20608f9827cc7cf7f2bd2f77f84b
abdou <abdosseyed@gmail.com>
2024-03-18 22:46:39 -0500

    Post view and route

--- Routes/PostsRoutes.js
+++ Routes/PostsRoutes.js
@@ -1,4 +1,14 @@
 import express from "express";
 const router = express.Router();
+const Post = require('../Models/Post');
+
+router.get('/post/:id', (req, res) => {
+    
+    let slug = req.params.id;
+
+    const data = Post.getPostById({_id, slug});
+
+    res.render('post', {data:data})
+})
 
 export default router;

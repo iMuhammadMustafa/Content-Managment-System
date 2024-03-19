e1f128b5d6b13bb9c96816b8fab67d1b57279224
iMuhammadMustafa <29302054+iMuhammadMustafa@users.noreply.github.com>
2024-03-19 16:54:48 -0500

    Fix Authpage layout
    - My formatter changed the lines spacing in some files sorry about that

--- Routes/AuthRoutes.js
+++ Routes/AuthRoutes.js
@@ -2,38 +2,37 @@ import express from "express";
 const router = express.Router();
 import cookieParser from "cookie-parser";
 
-
 import UsersRepo from "../Services/UsersRepo.js";
 import PostsRepo from "../Services/PostsRepo.js";
-router.get("/",(req, res) =>{
-    res.render("authentification.html", {title: "Authentication"});
-})
+router.get("/", (req, res) => {
+  res.render("authentification.html", { title: "Authentication", layout: "./Layouts/auth.ejs" });
+});
 
 router.post("/login", async (req, res) => {
-    let username = req.body.username;
-    let password = req.body.password;
-    let user = username ? await UsersRepo.getUserByUsername(username) : null;
-    if(!user || user.password !== password) {
-        res.status(404).send("Username or password is incorrect");
-    }
-    let topPosts = await PostsRepo.getTopPostsThisWeek();
-    let posts = await PostsRepo.getPosts();
-    res.cookie("user", user);
-    res.render("home.html",{title:"Home", posts, topPosts});
-})
+  let username = req.body.username;
+  let password = req.body.password;
+  let user = username ? await UsersRepo.getUserByUsername(username) : null;
+  if (!user || user.password !== password) {
+    res.status(404).send("Username or password is incorrect");
+  }
+  let topPosts = await PostsRepo.getTopPostsThisWeek();
+  let posts = await PostsRepo.getPosts();
+  res.cookie("user", user);
+  res.render("home.html", { title: "Home", posts, topPosts });
+});
 
 router.post("/register", async (req, res) => {
-    let username = req.body.username;
-    let password = req.body.password;
-    let email = req.body.email;
-    const currentDate = new Date();
+  let username = req.body.username;
+  let password = req.body.password;
+  let email = req.body.email;
+  const currentDate = new Date();
 
-    let user ={username, email, password, role : null , createdAt : currentDate, updatedAt : null};
-    let userCreated =  await UsersRepo.createUser(user);
-    let posts = await PostsRepo.getPosts();
-    let topPosts = await PostsRepo.getTopPostsThisWeek();
-    res.cookie("user", userCreated);
-    res.render("home.html",{title : "Home", posts, topPosts});
+  let user = { username, email, password, role: null, createdAt: currentDate, updatedAt: null };
+  let userCreated = await UsersRepo.createUser(user);
+  let posts = await PostsRepo.getPosts();
+  let topPosts = await PostsRepo.getTopPostsThisWeek();
+  res.cookie("user", userCreated);
+  res.render("home.html", { title: "Home", posts, topPosts });
 });
 
 export default router;
